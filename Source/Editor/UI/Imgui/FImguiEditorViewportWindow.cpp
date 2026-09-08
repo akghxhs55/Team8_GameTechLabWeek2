4285f3a76561bb313471551f0ec253c33570b7ac
Jaeho Yi <akghxhs55@gmail.com>
2026-09-09 02:49:08 +0900

    feat: 기즈모 단축키 추가

--- Source/Editor/UI/Imgui/FImguiEditorViewportWindow.cpp
+++ Source/Editor/UI/Imgui/FImguiEditorViewportWindow.cpp
@@ -85,7 +85,41 @@ void FImguiEditorViewportWindow::Process(FEditor& Editor,float DeltaTime)
 		{
 			FCamera& Camera = ActiveViewport->ViewportCamera;
 			CameraController.UpdateMouseInput(Camera);
-			CameraController.UpdateKeyInput(Camera, DeltaTime);
+			if (FInputManager::Get().IsMouseDown(EMouseButton::Right))
+			{
+				CameraController.UpdateKeyInput(Camera, DeltaTime);
+			}
+
+			if (!Gizmo.IsInteracting())
+			{
+				if (FInputManager::Get().IsKeyJustPressed(VK_OEM_3)) // 백틱 (`)
+				{
+					if (Gizmo.Mode != EGizmoMode::None && Gizmo.Mode != EGizmoMode::Scale)
+					{
+						Gizmo.SetGizmoSpace(static_cast<EGizmoSpace>((static_cast<uint8>(Gizmo.GetSpace()) + 1) % 2));
+					}
+				}
+				if (FInputManager::Get().IsKeyJustPressed('Q'))
+				{
+					Gizmo.Mode = EGizmoMode::None;
+				}
+				else if (FInputManager::Get().IsKeyJustPressed('W'))
+				{
+					Gizmo.Mode = EGizmoMode::Translate;
+				}
+				else if (FInputManager::Get().IsKeyJustPressed('E'))
+				{
+					Gizmo.Mode = EGizmoMode::Rotate;
+				}
+				else if (FInputManager::Get().IsKeyJustPressed('R'))
+				{
+					Gizmo.Mode = EGizmoMode::Scale;
+				}
+				else if (FInputManager::Get().IsKeyJustPressed(VK_SPACE))
+				{
+					Gizmo.Mode = static_cast<EGizmoMode>((static_cast<uint8>(Gizmo.Mode) + 1) % 4);
+				}
+			}
 		}
 	}
 
--- Source/Runtime/Input/FInputManager.cpp
+++ Source/Runtime/Input/FInputManager.cpp
@@ -102,12 +102,6 @@ void FInputManager::OnMouseButtonUp(EMouseButton Button, FVector2 Position)
 	PreviousMousePosition = Position;
 }
 
-void FInputManager::SetMousePos(FVector2 pos)
-{
-
-
-}
-
 FVector2 FInputManager::GetMousePosition() const
 {
 	return CurrentMousePosition;
