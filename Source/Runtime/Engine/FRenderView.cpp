6c58b22643788112cf2f7ced3614c0b78422e5cf
Jaeho Yi <akghxhs55@gmail.com>
2026-09-09 10:10:18 +0900

    feat: 선택된 오브젝트 하이라이팅

--- Source/Runtime/Engine/FRenderView.cpp
+++ Source/Runtime/Engine/FRenderView.cpp
@@ -1,22 +1,30 @@
 ﻿#include "FRenderView.h"
 
 #include "Runtime/Rendering/FRenderer.h"
+#include "Runtime/Rendering/ShaderConstants.h"
 #include "Runtime/Engine/FCamera.h"
 #include "Runtime/Math/FVector2.h"
 #include "Editor/Gizmo/FGizmo.h"
 #include "Editor/Grid/FGrid.h"
+
 FRenderView::FRenderView(FRenderer& Renderer):
 	Renderer(Renderer)
 {
 }
 
-void FRenderView::Render(const FCamera& Camera, FVector2 TopLeftUV, FVector2 LengthUV, UPrimitiveComponent* Rendered)
+void FRenderView::Render(const FCamera& Camera, FVector2 TopLeftUV, FVector2 LengthUV, UPrimitiveComponent* Rendered, bool bHighlighted)
 {
 	Renderer.SetViewportUV(TopLeftUV, LengthUV);
 	// TODO: 렌더뷰가 렌더러 구현을 알게 해서 여기서 V, P 따로 받고 월드축변환행렬을 곱하거나,
 	// 렌더러쪽 UpdateObjectConstants를 Draw함수 안에 숨긴뒤 인수로 M, V, P와 월드축을 받게 하면 렌더뷰도 렌더러 구현 모름
 	const FMatrix VP = Camera.CreateViewProjectionMatrix();
-	Renderer.Draw(*Rendered->GetMesh(), *Rendered->GetMaterial(), { Rendered->RelativeTransform.ToMatrix() * VP });
+	FObjectConstants Constants = { Rendered->RelativeTransform.ToMatrix() *VP };
+	if (bHighlighted)
+	{
+		Constants.ColorOverride = FVector{ 1.0f, 1.0f, 1.0f };
+		Constants.ColorOverrideAmount = 0.5f;
+	}
+	Renderer.Draw(*Rendered->GetMesh(), *Rendered->GetMaterial(), Constants);
 }
 
 void FRenderView::RenderGizmo(const FTransform& Transform, const FCamera& Camera, FVector2 TopLeftUV, FVector2 LengthUV, const FGizmo& Gizmo)
--- Source/Runtime/Engine/FRenderView.h
+++ Source/Runtime/Engine/FRenderView.h
@@ -15,7 +15,7 @@ class FRenderView final {
 
 public:
 	FRenderView(FRenderer& Renderer);
-	void Render(const FCamera& Camera, FVector2 TopLeftUV, FVector2 LengthUV, UPrimitiveComponent* Rendered);
+	void Render(const FCamera& Camera, FVector2 TopLeftUV, FVector2 LengthUV, UPrimitiveComponent* Rendered, bool bHighlighted = false);
 	void RenderGizmo(const FTransform& Transform, const FCamera& Camera, FVector2 TopLeftUV, FVector2 LengthUV, const FGizmo& Gizmo);
 	void RenderGrid(const FCamera& Camera, FVector2 TopLeftUV, FVector2 LengthUV, FGrid& Grid);
 };
